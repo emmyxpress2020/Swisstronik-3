c78fcd6b910f88ed75b8376ac7f62b249d4f302a
emmyxpress2020 <emmalex2222@gmail.com>
2024-07-30 10:14:58 +0000

    Initial commit

---
+++ scripts/mint.js
@@ -0,0 +1,35 @@
+const hre = require("hardhat");
+
+const { encryptDataField, decryptNodeResponse } = require("@swisstronik/utils");
+
+const sendShieldedTransaction = async (signer, destination, data, value) => {
+  const rpcLink = hre.network.config.url;
+  const [encryptedData] = await encryptDataField(rpcLink, data);
+  return await signer.sendTransaction({
+    from: signer.address,
+    to: destination,
+    data: encryptedData,
+    value,
+  });
+};
+
+async function main() {
+  const contractAddress = "0x1c9cc8741a618887e8325f3340C780D032e21a34"
+  const [signer] = await hre.ethers.getSigners();
+  const contractFactory = await hre.ethers.getContractFactory("ZunXBT");
+  const contract = contractFactory.attach(contractAddress);
+  const functionName = "safeMint";
+  const safeMintTx = await sendShieldedTransaction(
+    signer,
+    contractAddress,
+    contract.interface.encodeFunctionData(functionName, [signer.address, 1]),
+    0
+  );
+  await safeMintTx.wait();
+  console.log(`Transaction URL of Mint: https://explorer-evm.testnet.swisstronik.com/tx/${safeMintTx.hash}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
